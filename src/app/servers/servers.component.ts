0486df9ba7781d8f09d4b1a2a314d131b264843c
Prashant Sedhain <prashantased@gmail.com>
2020-02-16 16:44:51 -0600

    CHANGES
    Some Changes

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from "@angular/core";
   styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
+  clicked: boolean = false;
   name = "";
   age: number = 0;
   collegeName = "";
@@ -20,12 +21,31 @@ export class ServersComponent implements OnInit {
 
   ngOnInit() {}
 
-  onUpdateServerName(event: any) {
+  onUpdateRoommateCount(event: any) {
     this.numberOfRoommates = event.target.value;
   }
+  onUpdateName(event: any) {
+    this.name = event.target.value;
+  }
+
+  onUpdateAge(event: any) {
+    this.age = event.target.value;
+  }
+
+  onUpdateCollegeName(event: any) {
+    this.collegeName = event.target.value;
+  }
+
+  onUpdateAspiration(event: any) {
+    this.aspiration = event.target.value;
+  }
+
   onUpdateUserInfo(event: any) {}
 
   onContinueClicked() {
-    this.output = "You selected " + this.numberOfRoommates + " roommates.";
+    this.clicked = true;
+    if (this.age == 0) {
+      alert("You must input age.");
+    }
   }
 }
